ba71230eea2fc2770b6df10c396c47eed4db4c9a
Vedan10 <itsmevd1526@gmail.com>
2025-11-14 21:49:18 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating directories for project structure
+
+mkdir -p src 
+mkdir -p research
+
+
+# creating file 
+touch src/__init__.py
+touch src/helper.py
+# touch src/research/__init__.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "directories and files created successfully"
