651e73a1015bba52d0c3c4f50d490347dd201709
Vedan10 <itsmevd1526@gmail.com>
2025-11-18 22:40:44 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_pachages,setup
+
+setup(
+    name="Medical-chatbot",
+    version="0.1.0",
+    author="vedant",
+    author_email="vedantdhake1108@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
